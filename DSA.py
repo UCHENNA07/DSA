74125563026824b5edc13e1575d312061bc98ca7
Onwuka Uchenna <107627926+UCHENNA07@users.noreply.github.com>
2023-07-06 11:48:22 +0100

    DSA changes

--- DSA.py
+++ DSA.py
@@ -580,4 +580,111 @@
 # print(is_valid_parentheses(string2))
 
 
-# TREES
+# TREES AND BINARY SEARCH TREES
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+#
+#
+# class BinarySearchTree:
+#     def __init__(self):
+#         self.root = None
+#
+#     def insert(self, value):
+#         new_node = Node(value)
+#         if self.root is None:
+#             self.root = new_node
+#             return True
+#         temp = self.root
+#         while True:
+#             if new_node.value == temp.value:
+#                 return False
+#             if new_node.value < temp.value:
+#                 if temp.left is None:
+#                     temp.left = new_node
+#                     return True
+#                 temp = temp.left
+#             else:
+#                 if temp.right is None:
+#                     temp.right = new_node
+#                     return True
+#                 temp = temp.right
+#
+#     def contains(self, value):
+#         if self.root is None:
+#             return False
+#         temp = self.root
+#         while temp is not None:
+#             if value < temp.value:
+#                 temp = temp.left
+#             elif value > temp.value:
+#                 temp = temp.right
+#             else:
+#                 return True
+#         return False
+#
+#
+# my_tree = BinarySearchTree()
+# my_tree.insert(2)
+# my_tree.insert(1)
+# my_tree.insert(3)
+# my_tree.insert(27)
+#
+# #print(my_tree.root.value)
+# #print(my_tree.root.left.value)
+# #print(my_tree.root.right.value)
+#
+# print(my_tree.contains(27))
+# print(my_tree.contains(4))
+
+
+# HASH TABLES
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None] * size
+
+    def hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+
+    def set_item(self, key, value):
+        index = self.hash(key)
+        if self.data_map[index] is None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
+
+my_hash_table = HashTable()
+my_hash_table.set_item('bolts', 1400)
+my_hash_table.set_item('washers', 50)
+my_hash_table.set_item('lumber', 70)
+
+# print(my_hash_table.get_item('bolts'))
+# print(my_hash_table.get_item('washers'))
+# print(my_hash_table.get_item('lumber'))
+
+print(my_hash_table.keys())
