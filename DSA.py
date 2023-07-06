7f263a10c449e384890a18c7fcc33f5ce920fcf4
Onwuka Uchenna <107627926+UCHENNA07@users.noreply.github.com>
2023-07-06 13:01:35 +0100

    DSA changes

--- DSA.py
+++ DSA.py
@@ -641,50 +641,83 @@
 
 
 # HASH TABLES
-class HashTable:
-    def __init__(self, size=7):
-        self.data_map = [None] * size
-
-    def hash(self, key):
-        my_hash = 0
-        for letter in key:
-            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
-        return my_hash
-
-    def print_table(self):
-        for i, val in enumerate(self.data_map):
-            print(i, ": ", val)
-
-    def set_item(self, key, value):
-        index = self.hash(key)
-        if self.data_map[index] is None:
-            self.data_map[index] = []
-        self.data_map[index].append([key, value])
-
-    def get_item(self, key):
-        index = self.hash(key)
-        if self.data_map[index] is not None:
-            for i in range(len(self.data_map[index])):
-                if self.data_map[index][i][0] == key:
-                    return self.data_map[index][i][1]
-        return None
-
-    def keys(self):
-        all_keys = []
-        for i in range(len(self.data_map)):
-            if self.data_map[i] is not None:
-                for j in range(len(self.data_map[i])):
-                    all_keys.append(self.data_map[i][j][0])
-        return all_keys
-
-
-my_hash_table = HashTable()
-my_hash_table.set_item('bolts', 1400)
-my_hash_table.set_item('washers', 50)
-my_hash_table.set_item('lumber', 70)
+# class HashTable:
+#     def __init__(self, size=7):
+#         self.data_map = [None] * size
+#
+#     def hash(self, key):
+#         my_hash = 0
+#         for letter in key:
+#             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+#         return my_hash
+#
+#     def print_table(self):
+#         for i, val in enumerate(self.data_map):
+#             print(i, ": ", val)
+#
+#     def set_item(self, key, value):
+#         index = self.hash(key)
+#         if self.data_map[index] is None:
+#             self.data_map[index] = []
+#         self.data_map[index].append([key, value])
+#
+#     def get_item(self, key):
+#         index = self.hash(key)
+#         if self.data_map[index] is not None:
+#             for i in range(len(self.data_map[index])):
+#                 if self.data_map[index][i][0] == key:
+#                     return self.data_map[index][i][1]
+#         return None
+#
+#     def keys(self):
+#         all_keys = []
+#         for i in range(len(self.data_map)):
+#             if self.data_map[i] is not None:
+#                 for j in range(len(self.data_map[i])):
+#                     all_keys.append(self.data_map[i][j][0])
+#         return all_keys
+#
+#
+# my_hash_table = HashTable()
+# my_hash_table.set_item('bolts', 1400)
+# my_hash_table.set_item('washers', 50)
+# my_hash_table.set_item('lumber', 70)
 
 # print(my_hash_table.get_item('bolts'))
 # print(my_hash_table.get_item('washers'))
 # print(my_hash_table.get_item('lumber'))
+#
+# print(my_hash_table.keys())
 
-print(my_hash_table.keys())
+
+# Common interview question
+# # this first method is the inefficient way to code this because it is O(n^2)
+# def items_in_common(list1, list2):
+#     for i in list1:
+#         for j in list2:
+#             if i == j:
+#                 return True
+#     return False
+#
+#
+# list1 = [1, 3, 5]
+# list2 = [2, 4, 5]
+#
+# print(items_in_common(list1, list2))
+
+# This second method is the most efficient way to code this because its O(n)
+def items_in_common(list1, list2):
+    my_dict = {}
+    for i in list1:
+        my_dict[i] = True
+    for j in list2:
+        if j in my_dict:
+            return True
+    return False
+
+
+list1 = [1, 3, 5]
+list2 = [2, 4, 5]
+
+print(items_in_common(list1, list2))
+
