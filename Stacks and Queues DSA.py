e97c8b7a222e210d0baf1879fc25cdcfbe3562b0
Onwuka Uchenna <107627926+UCHENNA07@users.noreply.github.com>
2023-08-30 17:49:07 +0100

    Update Stacks and Queues DSA.py

--- Stacks and Queues DSA.py
+++ Stacks and Queues DSA.py
@@ -1,97 +1,99 @@
 # STACK AND QUEUES
-# class Node:
-#     def __init__(self, value):
-#         self.value = value
-#         self.next = None
-#
-#
-# class Stack:
-#     def __init__(self, value):
-#         new_node = Node(value)
-#         self.top = new_node
-#         self.height = 1
-#
-#     def print_stack(self):
-#         temp = self.top
-#         while temp is not None:
-#             print(temp.value)
-#             temp = temp.next
-#
-#     def push(self, value):
-#         new_node = Node(value)
-#         if self.height == 0:
-#             self.top = new_node
-#         else:
-#             new_node.next = self.top
-#             self.top = new_node
-#         self.height += 1
-# #
-#     def pop(self):
-#         if self.height == 0:
-#             return None
-#         temp = self.top
-#         self.top = self.top.next
-#         temp.next = None
-#         self.height -= 1
-#         return temp
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
-# my_stack = Stack(3)
-# my_stack.push(4)
-# my_stack.push(5)
-# my_stack.pop()
-# my_stack.print_stack()
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
 
 
-# QUEUES
-# class Node:
-#     def __init__(self, value):
-#         self.value = value
-#         self.next = None
-#
-#
-# class Queue:
-#     def __init__(self, value):
-#         new_node = Node(value)
-#         self.first = new_node
-#         self.last = new_node
-#         self.length = 1
-#
-#     def print_queue(self):
-#         temp = self.first
-#         while temp:
-#             print(temp.value)
-#             temp = temp.next
-#
-#     def enqueue(self, value):
-#         # This method appends a new node to the end of a queue
-#         new_node = Node(value)
-#         if self.length == 0:
-#             self.first = new_node
-#             self.last = new_node
-#         else:
-#             self.last.next = new_node
-#             self.last = new_node
-#         self.length += 1
-#
-#     def dequeue(self):
-#         # This method removes the node at the beginning of the queue
-#         if self.length == 0:
-#             return None
-#         temp = self.first
-#         if self.length < 2:
-#             self.first = None
-#             self.last = None
-#         else:
-#             self.first = self.first.next
-#             temp.next = None
-#         self.length -= 1
-#         return temp
-#
-#
-# my_queue = Queue(1)
-# my_queue.enqueue(2)
-# my_queue.enqueue(3)
-# my_queue.dequeue()
-# my_queue.print_queue()
+my_stack = Stack(3)
+my_stack.push(4)
+my_stack.push(5)
+my_stack.pop()
+my_stack.print_stack()
+
+
+QUEUES
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def enqueue(self, value):
+        # This method appends a new node to the end of a queue
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+    def dequeue(self):
+        # This method removes the node at the beginning of the queue
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length < 2:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
+
+my_queue = Queue(1)
+my_queue.enqueue(2)
+my_queue.enqueue(3)
+my_queue.dequeue()
+my_queue.print_queue()
+
+
 
