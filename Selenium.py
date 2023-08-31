96bd5be21d4f251de222595398c612797c87dd41
Onwuka Uchenna <107627926+UCHENNA07@users.noreply.github.com>
2023-08-31 09:57:18 +0100

    Initial commit

---
+++ Selenium.py
@@ -0,0 +1,13 @@
+# from selenium import webdriver
+#
+# chrome_driver_path = r"C:\Users\MY-PC\Documents\chromedriver_win32\chromedriver.exe"
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+#
+# driver.get("https://www.amazon.com")
+#
+# price = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div[2]/div/div[8]/div/div/div[2]/div[2]/div/div/div[6]/div/div[3]/div[2]/div/span[2]")
+# print(price.text)
+#
+# driver.quit()
+
+
